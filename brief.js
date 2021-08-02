0918e28692253f273372b9231af38799aafa6e48
moh31 <31807652+moh31@users.noreply.github.com>
2021-08-02 17:34:32 +0100

    Create brief.js

---
+++ brief.js
@@ -0,0 +1,6 @@
+ <script type='text/javascript'>  
+//<![CDATA[  
+/* Night Mode/Dark Mode Feature - By Arabes1.com */    
+$(document).ready(function(){$("body").toggleClass(localStorage.toggled),$("#arabes1").on("click",function(){"nightmode"!=localStorage.toggled?($("body").toggleClass("nightmode",!0),localStorage.toggled="nightmode",$(".section-center").css("display","block")):($("body").toggleClass("nightmode",!1),localStorage.toggled="",$(".section-center").css("display",""))}),$("body").hasClass("nightmode")?$("#arabes1").prop("checked",!0):$("#arabes1").prop("checked",!1)});  
+//]]>  
+</script>
